5e2de97b1a34f9fdab0d697959fb9d2e476f3a9f
saylon147 <saylon@thebingo.cc>
2026-09-22 10:13:11 +0800

    edit bat

--- check_duplicates.bat
+++ check_duplicates.bat
@@ -17,42 +17,41 @@ set "HASHLIST=%TEMP%\duplicate_hash_%RANDOM%_%RANDOM%.txt"
 type nul > "%HASHLIST%"
 
 for %%F in (*) do (
+
+    rem Skip this BAT file itself
     if /i not "%%~nxF"=="%~nx0" (
+
+        rem Only process files
         if not "%%~aF:~0,1"=="d" (
 
             set "hash="
 
-            for /f "tokens=1" %%H in ('certutil -hashfile "%%F" SHA256 ^| findstr /r /v "hash CertUtil"') do (
+            for /f "tokens=1" %%H in (
+                'certutil -hashfile "%%F" SHA256 ^| findstr /r /v "hash CertUtil"'
+            ) do (
                 set "hash=%%H"
             )
 
             if defined hash (
+
                 findstr /b /i /c:"!hash!|" "%HASHLIST%" >nul
 
                 if !errorlevel! equ 0 (
+
                     echo [DUPLICATE] %%F
 
-                    set "TARGET=%DUPDIR%\%%~nxF"
-
-                    if exist "!TARGET!" (
-                        set /a N=1
-
-                        :CHECK_NAME
-                        set "TARGET=%DUPDIR%\%%~nF_!N!%%~xF"
-
-                        if exist "!TARGET!" (
-                            set /a N+=1
-                            goto CHECK_NAME
-                        )
-                    )
+                    call :GetSafeName "%%~nF" "%%~xF"
 
                     move "%%F" "!TARGET!" >nul
 
                     echo             -^> !TARGET!
                     echo.
+
                 ) else (
+
                     echo !hash!^|%%F>>"%HASHLIST%"
                     echo [KEEP]      %%F
+
                 )
             )
         )
@@ -68,3 +67,27 @@ echo Duplicate files moved to:
 echo %CD%\%DUPDIR%
 echo ========================================
 pause
+exit /b
+
+
+:GetSafeName
+
+set "BASENAME=%~1"
+set "EXT=%~2"
+
+set "TARGET=%DUPDIR%\%BASENAME%%EXT%"
+
+if not exist "%TARGET%" exit /b
+
+set /a N=1
+
+:TryNextName
+
+set "TARGET=%DUPDIR%\%BASENAME%_%N%%EXT%"
+
+if exist "%TARGET%" (
+    set /a N+=1
+    goto TryNextName
+)
+
+exit /b
